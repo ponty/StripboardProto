185d80a8a287049e0e3d50ae11f3d185ddbabfaf
ponty <ponty@home>
2017-01-23 07:55:05 +0100

    conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,8 +17,9 @@ extensions = [
     'sphinxcontrib.eagle',
     #'sphinx.ext.autodoc',
     #'sphinxcontrib.programoutput',
-    'sphinx.ext.graphviz',
+#     'sphinx.ext.graphviz',
     #'sphinx.ext.autosummary',
+    'rst2pdf.pdfbuilder',
 ]
 # intersphinx_mapping = {'http://docs.python.org/': None,
 #'http://packages.python.org/sphinxcontrib-programoutput/' : None,
@@ -28,10 +29,10 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 
-exclude_patterns = ['_build/*']
+# exclude_patterns = ['_build/*']
 
 html_theme = 'default'
-html_static_path = []
+# html_static_path = []
 
 # pdf_break_level = 2
 
@@ -39,22 +40,31 @@ html_static_path = []
 #    'ansi': ('http://packages.python.org/sphinxcontrib-ansi', None)}
 
 
-def setup(app):
-    app.add_description_unit('confval', 'confval',
-                             'pair: %s; configuration value')
+# def setup(app):
+#     app.add_description_unit('confval', 'confval',
+#                              'pair: %s; configuration value')
 
 
 # latex build settings
-latex_documents = [
-    ('index', '%s.tex' % project, u'%s Documentation' % project,
-     author, 'manual'),
-]
+# latex_documents = [
+#     ('index', '%s.tex' % project, u'%s Documentation' % project,
+#      author, 'manual'),
+# ]
 
 # remove blank pages from pdf
 # http://groups.google.com/group/sphinx-
 # dev/browse_thread/thread/92e19267d095412d/d60dcba483c6b13d
-latex_font_size = '10pt,oneside'
+#latex_font_size = '10pt,oneside'
+#WARNING: latex_font_size is deprecated. Use latex_elements['pointsize'] instead.
 
 latex_elements = dict(
     papersize='a4paper',
 )
+
+
+# https://www.quora.com/How-to-create-a-PDF-out-of-Sphinx-documentation-tool
+pdf_documents = [('index', project, project, author),]
+# index - master document
+# rst2pdf - name of the generated pdf
+# Sample rst2pdf doc - title of the pdf
+# Your Name - author name in the pdf
